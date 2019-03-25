10ff6b2eef18b7068c92f5e735aec6d9c8eb373a
yuanling <yuanling@cnwansun.com>
2019-03-25 18:10:50 +0800

    locationMessage

---
+++ wansun.visit.android/src/main/java/wansun/visit/android/bean/saveLocationMessageBean.java
@@ -0,0 +1,8 @@
+package wansun.visit.android.bean;
+
+/**
+ * Created by User on 2019/3/25.
+ */
+
+public class saveLocationMessageBean {
+}
---
+++ wansun.visit.android/src/main/java/wansun/visit/android/boardCastReceiver/BootCompletedReceiver.java
@@ -0,0 +1,8 @@
+package wansun.visit.android.boardCastReceiver;
+
+/**
+ * Created by User on 2019/3/25.
+ */
+
+public class BootCompletedReceiver {
+}
